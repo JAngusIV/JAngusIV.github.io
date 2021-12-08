51a5a9cb8953c2dec2cb4d2425eb926f1cf829c0
JAngusIV <80146348+JAngusIV@users.noreply.github.com>
2021-12-07 21:49:53 -0800

    joke.js comments

--- jokes.js
+++ jokes.js
@@ -1,15 +1,24 @@
+// Looks for window size of 500px width and less
+var x = window.matchMedia("(max-width: 501px)")
+window.onload = mediaQuery(x); // Call listener function at run time
+// Actively watches for changed screen size
+x.addEventListener("change", mediaQuery)
 
+
+
+// Initializes variables for when mouse hovers over button and clicks button
 var hoverCount = 0;
 var clickCount = 0;
 
-
+// Initializes the text contents of Joke div and button, and prepares each for further input.
 const jokeButton = document.querySelector(".getJoke");
 const jokeHolder = document.querySelector(".joke");
 
+/* Creates array for text that will fill in button each time it is pressed */
 const buttonText = 
 [
-  "ugh.",
-  "omg dad.",
+  "ugh",
+  "omg dad",
   "you are the worst",
   "seriously",
   "stop it",
@@ -19,21 +28,29 @@ const buttonText =
   "stooooop",
   "this has to end",
   "you must be tired of this",
+  "o m g",
+  "i cant even right now",
+  "pls",
+  "ok that one was alright",
+  "what year is it?"
 ];
 
-
-
-
-
- 
-/* Hovering over button will cause the button's color to change to a randomly generated color */
-function changeColor()
+/* Function to call a random item from an array */
+// Will not repeat the same item twice.
+function randomItemFromArray(arr, not) 
 {
-    document.getElementById('button1').style.backgroundColor = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);  
- 
+    const item = arr[Math.floor(Math.random() * arr.length)];
+
+    if (item == not) 
+    {
+        console.log("Ah! we used that one last time, look again");
+        return randomItemFromArray(arr, not);
+    }
+        
+    return item;
 }
 
-
+/* API query for generating a random Dad Joke */
 async function fetchJoke() 
 {
   const response = await fetch("https://icanhazdadjoke.com", 
@@ -48,6 +65,10 @@ async function fetchJoke()
     return data;
 }
 
+/* Function for when user clicks button */
+// Calls fetchJoke and generates text for random Dad Joke.
+// Calls a random item from buttonText to replace text read on button with each click.
+// Increments clickCount by one on successful button press.
 async function handleClick() 
 {
     const { joke } = await fetchJoke();
@@ -60,55 +81,42 @@ async function handleClick()
     );
 
     clickCount++;
-
 }
 
-function randomItemFromArray(arr, not) 
+
+/* When button is successfully clicked, call function handleClick */
+jokeButton.addEventListener("click", handleClick);
+
+
+/* Function for hovering over button will cause the button's color to change to a randomly generated color */
+function changeColor()
 {
-    const item = arr[Math.floor(Math.random() * arr.length)];
-
-    if (item == not) 
-    {
-        console.log("Ah! we used that one last time, look again");
-        return randomItemFromArray(arr, not);
-    }
-        
-    return item;
+    document.getElementById('button1').style.backgroundColor = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);  
+ 
 }
 
-
-
-
-
-
+/* Function to scan window size */
+// If window size is 500px or below, background becomes pink and button will remain static.
+// If window size is greater than 500px, button will hop to a random position on mouse hover.
 function mediaQuery(x) {
 
-    if (x.matches) { // If media query matches
-      document.body.style.backgroundColor = "#bcbcbc";
-    } else {
-     document.body.style.backgroundColor = "white";
+  if (x.matches) { // If media query matches
+    document.body.style.backgroundColor = "#bcbcbc";
+  } else {
+   document.body.style.backgroundColor = "white";
 
-     /* Makes button move at random when hovered over */
-     $(function() {
-        $("button").on({
-          mouseover: function() {
-            $(this).css({
-              left: (-10 + Math.random() * 20) + "em",
-              top: (-10 + Math.random() * 20) + "em", 
-            });
-            changeColor()
-          }
-        });
+   /* Makes button move at random when hovered over */
+   $(function() {
+      $("button").on({
+        mouseover: function() {
+          $(this).css({
+            left: (-10 + Math.random() * 20) + "em",
+            top: (-10 + Math.random() * 20) + "em", 
+          });
+          changeColor()
+        }
       });
-    }
+    });
   }
+}
 
-// Looks for window size of 500px width and less
-var x = window.matchMedia("(max-width: 501px)")
-window.onload = mediaQuery(x); // Call listener function at run time
-// Actively watches for changed screen size
-x.addEventListener("change", mediaQuery)
-
-
-/* When button is clicked, call function handleClick */
-jokeButton.addEventListener("click", handleClick);
